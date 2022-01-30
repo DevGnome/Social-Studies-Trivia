9a7224b50949b7dd65ddca307f04186b8a547708
Hugh Witchella <hjwitchellacoding@gmail.com>
2022-01-29 20:27:09 -0500

    Set a basic HTML doc to edit later. JS test function to check data retrieved correctly

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+// URL for Chosen API 
+const BASE_URL = "https://opentdb.com/api.php?amount=10&category=23&type=multiple"
 
+//DOMContentLoaded Listener.
+window.addEventListener("DOMContentLoaded", () =>{
+
+})
+
+//Function containing Fetch request. This will be a GET request that returns a PROMISE objoect
+function APIFetch(){
+    fetch(BASE_URL)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+APIFetch();
