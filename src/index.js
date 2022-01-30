5786f49536882368fb48bb5a51c37f48dc547a85
Hugh Witchella <hjwitchellacoding@gmail.com>
2022-01-30 14:49:33 -0500

    Made containers for answer choices. Starting collection of elements and variables.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
-// URL for Chosen API 
+//URL for Chosen API 
 const BASE_URL = "https://opentdb.com/api.php?amount=10&category=23&type=multiple"
+
+
+//Element Variables
+const question = document.getElementById('question');
+const answers = Array.from(document.getElementsByClassName('answer-choices'));
+// Counter for Questions, i.e. 'Questions 1/10'
+// Counter for Correct Answers.
+
+
+
 
 //DOMContentLoaded Listener.
 window.addEventListener("DOMContentLoaded", () =>{
-
+    fetch(BASE_URL)
+    .then(response => response.json)
+    .then()
 })
 
 //Function containing Fetch request. This will be a GET request that returns a PROMISE objoect
-function APIFetch(){
-    fetch(BASE_URL)
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
-
-APIFetch();
+// function APIFetch(){
+//     fetch(BASE_URL)
+//     .then(response => response.json())
+//     .then(data => console.log(data));
+// }
+//APIFetch();
