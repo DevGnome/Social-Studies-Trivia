81b89b6adbdca478a4bc8f20e5dddd49aef7aca6
Hugh Witchella <hjwitchellacoding@gmail.com>
2022-01-30 16:49:22 -0500

    created buttons, need to randomize answer buttons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 //URL for Chosen API 
-const BASE_URL = "https://opentdb.com/api.php?amount=10&category=23&type=multiple"
-
-
-//Element Variables
-const question = document.getElementById('question');
-const answers = Array.from(document.getElementsByClassName('answer-choices'));
+const BASE_URL = "https://opentdb.com/api.php?amount=10&category=18&type=multiple"
 // Counter for Questions, i.e. 'Questions 1/10'
 // Counter for Correct Answers.
 
@@ -14,14 +9,17 @@ const answers = Array.from(document.getElementsByClassName('answer-choices'));
 //DOMContentLoaded Listener.
 window.addEventListener("DOMContentLoaded", () =>{
     fetch(BASE_URL)
-    .then(response => response.json)
-    .then()
+    .then(response => response.json())
+    .then(data => renderQuestions(data))
+    // .then(data=> console.log(data))
 })
 
-//Function containing Fetch request. This will be a GET request that returns a PROMISE objoect
-// function APIFetch(){
-//     fetch(BASE_URL)
-//     .then(response => response.json())
-//     .then(data => console.log(data));
-// }
-//APIFetch();
+//Function that Renders the Question and answers
+function renderQuestions(data){
+    document.querySelector("#question").innerHTML = `${data.results[0].question}`
+    document.getElementById('option1').innerHTML = `${data.results[0].correct_answer}`
+    document.getElementById('option2').innerHTML = `${data.results[0].incorrect_answers[0]}`
+    document.getElementById('option3').innerHTML = `${data.results[0].incorrect_answers[1]}`
+    document.getElementById('option4').innerHTML = `${data.results[0].incorrect_answers[2]}`
+}
+//Function to get next question
