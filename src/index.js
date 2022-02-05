1a9e5f34b15b237576e58c2200b6a9a965340f9e
Hugh Witchella <hjwitchellacoding@gmail.com>
2022-02-05 13:52:52 -0500

    retructured to be a single question to simplify

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
 //URL for Chosen API 
-const BASE_URL = "https://opentdb.com/api.php?amount=10&category=18&type=multiple"
-// Counter for Questions, i.e. 'Questions 1/10'
-// Counter for Correct Answers.
+const BASE_URL = "https://opentdb.com/api.php?amount=1&category=18&type=multiple"
+
 
 
 
 
 //DOMContentLoaded Listener.
 window.addEventListener("DOMContentLoaded", () =>{
+    getQuestion();
+   
+})
+//Get Question Function
+function getQuestion(){
     fetch(BASE_URL)
     .then(response => response.json())
-    .then(data => renderQuestions(data))
-    // .then(data=> console.log(data))
-})
-
-//Function that Renders the Question and answers
-function renderQuestions(data){
-    document.querySelector("#question").innerHTML = `${data.results[0].question}`
-    document.getElementById('answer-text1').innerHTML = `${data.results[0].correct_answer}`;
-    document.getElementById('answer-text2').innerHTML = `${data.results[0].incorrect_answers[0]}`;
-    document.getElementById('answer-text3').innerHTML = `${data.results[0].incorrect_answers[1]}`;
-    document.getElementById('answer-text4').innerHTML = `${data.results[0].incorrect_answers[2]}`;
+    // .then(data => renderQuestion(data))
+    .then(data => console.log(data))
 }
+//Function that Renders the Question and answers
+function renderQuestion(data){
+    document.getElementById("question").innerHTML = `${data.results[0].question}`
+
+}    
+const answerChoices = []
+//Function for EventListener.
+//If -> correct answer button clicked -> Correct! Maybe change button color?
+//Else -> Incorrect! maybe change button color red. 
+//-------->Prevent Clicking of Buttons AFTER choice made<----------
 //Function to get next question
