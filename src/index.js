0f2ff4e74b81b3a9e6fbe7ac97bf3e416d024653
Hugh Witchella <hjwitchellacoding@gmail.com>
2022-02-05 14:13:54 -0500

    randomized array for answers. Need to check for correct answer next

--- src/index.js
+++ src/index.js
@@ -14,15 +14,25 @@ window.addEventListener("DOMContentLoaded", () =>{
 function getQuestion(){
     fetch(BASE_URL)
     .then(response => response.json())
-    // .then(data => renderQuestion(data))
-    .then(data => console.log(data))
+    .then(data => renderQuestion(data))
+    // .then(data => console.log(data))
 }
 //Function that Renders the Question and answers
 function renderQuestion(data){
+    let answerChoices = [...data.results[0].incorrect_answers]
+    data.results[0].answer = Math.floor(Math.random()*4)+1;
+    answerChoices.splice(data.results[0].answer - 1, 0, data.results[0].correct_answer);
+    console.log(answerChoices);
+    //Display Question
     document.getElementById("question").innerHTML = `${data.results[0].question}`
-
+    //Display Answers Choices
+    document.getElementById("answer-text1").innerHTML = `${answerChoices[0]}`
+    document.getElementById("answer-text2").innerHTML = `${answerChoices[1]}`
+    document.getElementById("answer-text3").innerHTML = `${answerChoices[2]}`
+    document.getElementById("answer-text4").innerHTML = `${answerChoices[3]}`
 }    
-const answerChoices = []
+
+
 //Function for EventListener.
 //If -> correct answer button clicked -> Correct! Maybe change button color?
 //Else -> Incorrect! maybe change button color red. 
