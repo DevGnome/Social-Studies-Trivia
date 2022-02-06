32878a39c7d815cee26922501fd1b46788d85432
Hugh Witchella <hjwitchellacoding@gmail.com>
2022-02-06 00:31:23 -0500

    added in some notes on JS used and comments on future additions wanted

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // Make Webpage a "Quiz". Load a question, provide answers, refresh after answer.
 // Load DOM -> Fetch API -> Listen for Click -> Check Event -> Correct? Y/N Text -> New Question Button
 // 
+// Guideline Stretch Goal: db.json
 // 
-// 
-// 
+// Personal Stretch Goal: Way to Change Category
 // 
 // 
 // 
@@ -26,16 +26,19 @@ window.addEventListener("DOMContentLoaded", () =>{
                 if(e.target.innerHTML === document.getElementById("correct-answer").innerHTML
                 ){
                     document.getElementById("result").innerHTML = "That Is Correct!"
-                    document.getElementById('result').style.backgroundColor="green"
+                    document.getElementById('result').style.cssText = `background-color: green; border: 1px solid black; padding-left: 2px;
+                    padding-right: 2px;`
                 }
                 else{
                     document.getElementById("result").innerHTML = "Try Again!"
-                    document.getElementById('result').style.backgroundColor="red"
+                    document.getElementById('result').style.cssText=`background-color: red; border: 1px solid black; padding-left: 2px;
+                    padding-right: 2px;`
 
                 }
             }
         })
     }
+
 });
 
 
@@ -49,17 +52,21 @@ function getQuestion(){
 }
 //Function that Renders the Question and Answers
 function renderQuestion(data){
-
+    //Create array containing incorrect answers
     const answerChoices = [...data.results[0].incorrect_answers]
     const correctAnswer = data.results[0].correct_answer;
+    //Get a random index for answer choices
     data.results[0].answer = Math.floor(Math.random()*4)+1;
+    //Combine incorrect and correct answers into array to randomize
+    //for the answer display.
     answerChoices.splice(data.results[0].answer - 1, 0, correctAnswer);
+    //Logs all answers and specificall the Correct one.
     console.log(answerChoices);
     console.log(correctAnswer);
 
     //Display Question
     document.getElementById("question").innerHTML = `${data.results[0].question}`
-    //Display Answers Choices
+    //Display Answers Choices --- Could be cleaner
     document.getElementById("answer-text1").innerHTML = `${answerChoices[0]}`
     document.getElementById("answer-text2").innerHTML = `${answerChoices[1]}`
     document.getElementById("answer-text3").innerHTML = `${answerChoices[2]}`
@@ -67,20 +74,4 @@ function renderQuestion(data){
     //Put correctAnswer in a hidden tag because I can't figure out another way to get the data into eventlistener
     document.getElementById("correct-answer").innerHTML = `${data.results[0].correct_answer}`;
 }    
-//EventListener for Answer button being Clicked
-// function handleAnswer(){
-//     addEventListener("click", (e)=>{
 
-//         if(e.target.className === 'option'){
-//             console.log(e.target.innerHTML);
-//             document.getElementById("new-question").disabled = false;
-
-//             if(e.target.innerHTML === checkCorrect){
-//                 document.getElementById("result").innerHTML = "That Is Correct!"
-//             }
-//             else{
-//                 document.getElementById("result").innerHTML = "Correct Answer:"
-//             }
-//         }
-//     })
-// }
