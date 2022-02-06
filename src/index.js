30b41641704e396d725f51eca8a75523c4fa45ae
Hugh Witchella <hjwitchellacoding@gmail.com>
2022-02-05 23:09:46 -0500

    if button is clicked it highlights white. Result highlights green for correct, red for incorrect

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
+// Make Webpage a "Quiz". Load a question, provide answers, refresh after answer.
+// Load DOM -> Fetch API -> Listen for Click -> Check Event -> Correct? Y/N Text -> New Question Button
+// 
+// 
+// 
+// 
+// 
+// 
+// 
 //URL for Chosen API 
 const BASE_URL = "https://opentdb.com/api.php?amount=1&category=18&type=multiple"
-
-
-
-
 
 //DOMContentLoaded Listener.
 window.addEventListener("DOMContentLoaded", () =>{
     //Disable New Question Button until answer chosen
     document.getElementById("new-question").disabled = true;
     getQuestion();
-    handleClick();
+    handleAnswer();
+    function handleAnswer(){
+        addEventListener("click", (e)=>{
+    
+            if(e.target.className === 'option'){
+                console.log(e.target.innerHTML);
+                document.getElementById("new-question").disabled = false;
+    
+                if(e.target.innerHTML === document.getElementById("correct-answer").innerHTML
+                ){
+                    document.getElementById("result").innerHTML = "That Is Correct!"
+                    document.getElementById('result').style.backgroundColor="green"
+                }
+                else{
+                    document.getElementById("result").innerHTML = "Try Again!"
+                    document.getElementById('result').style.backgroundColor="red"
 
+                }
+            }
+        })
+    }
 });
 
 
@@ -20,15 +44,19 @@ function getQuestion(){
     fetch(BASE_URL)
     .then(response => response.json())
     .then(data => renderQuestion(data))
-    // .then(data => console.log(data))
+    // .then(data => console.log(data);
+
 }
 //Function that Renders the Question and Answers
 function renderQuestion(data){
-    let answerChoices = [...data.results[0].incorrect_answers]
-    let correctChoice = data.results[0].correct_answer;
+
+    const answerChoices = [...data.results[0].incorrect_answers]
+    const correctAnswer = data.results[0].correct_answer;
     data.results[0].answer = Math.floor(Math.random()*4)+1;
-    answerChoices.splice(data.results[0].answer - 1, 0, correctChoice);
+    answerChoices.splice(data.results[0].answer - 1, 0, correctAnswer);
     console.log(answerChoices);
+    console.log(correctAnswer);
+
     //Display Question
     document.getElementById("question").innerHTML = `${data.results[0].question}`
     //Display Answers Choices
@@ -36,31 +64,23 @@ function renderQuestion(data){
     document.getElementById("answer-text2").innerHTML = `${answerChoices[1]}`
     document.getElementById("answer-text3").innerHTML = `${answerChoices[2]}`
     document.getElementById("answer-text4").innerHTML = `${answerChoices[3]}`
+    //Put correctAnswer in a hidden tag because I can't figure out another way to get the data into eventlistener
+    document.getElementById("correct-answer").innerHTML = `${data.results[0].correct_answer}`;
 }    
+//EventListener for Answer button being Clicked
+// function handleAnswer(){
+//     addEventListener("click", (e)=>{
 
-//Function for EventListener
-function handleClick(){
-    document.addEventListener("click", (e)=>{
-        let chosen = e.target;
-        //So far this is the only way I've been able to disable all butons on press.
-        //Will revisit with better solution
-        document.getElementById("btn1").disabled = true;
-        document.getElementById("btn2").disabled = true;
-        document.getElementById("btn3").disabled = true;
-        document.getElementById("btn4").disabled = true;
-        document.getElementById("new-question").disabled = false;
+//         if(e.target.className === 'option'){
+//             console.log(e.target.innerHTML);
+//             document.getElementById("new-question").disabled = false;
 
-        if(chosen.type === "answer-button" && chosen.innerHTML === correctChoice){
-
-                document.getElementById("result").innerHTML = "That Is Correct"
-            }
-            else{
-                document.getElementById("result").innerHTML = "Sorry, That Is Incorrect"
-            }
-        })
-        
-}
-//If -> correct answer button clicked -> Correct! Maybe change button color?
-//Else -> Incorrect! maybe change button color red. 
-//-------->Prevent Clicking of Buttons AFTER choice made<----------
-//Function to get next question
+//             if(e.target.innerHTML === checkCorrect){
+//                 document.getElementById("result").innerHTML = "That Is Correct!"
+//             }
+//             else{
+//                 document.getElementById("result").innerHTML = "Correct Answer:"
+//             }
+//         }
+//     })
+// }
